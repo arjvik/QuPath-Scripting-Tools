34c8f95b94bafd89f63e12b19b0047fa23aee76a
Arjun Vikram <arjvik@gmail.com>
2020-07-21 17:30:45 -0500

    Add kill switch to RemoteCodeClient

--- src/RemoteCodeClient.groovy
+++ src/RemoteCodeClient.groovy
@@ -5,26 +5,34 @@ final PORT = 6666
 args = ["src/HelloWorld.groovy"]
 // END DEBUG ONLY
 
-def file = new File(args[0])
-def codeLines = 0
-file.eachLine { codeLines++ }
-
 def client = new Socket(InetAddress.localHost, 6666)
-def request = new PrintStream(client.outputStream)
-def response = new BufferedReader(new InputStreamReader(client.inputStream))
-
-assert response.readLine() == "WELCOME"
-request.println "RUN ${codeLines}"
-file.eachLine { request.println it }
-assert response.readLine() == "RECIEVED"
-
-def status = response.readLine().split(" (?:: )?", 2)
-if (status[0] == "SUCCESS") {
-	response.eachLine { println it }
-} else if (status[0] == "EXECUTION-ERROR") {
-	System.err.println "Runtime error: ${status[1]}"
-} else {
-	System.err.println("Unknown error (status: ${status})")
+try {
+	def request = new PrintStream(client.outputStream)
+	def response = new BufferedReader(new InputStreamReader(client.inputStream))
+	
+	assert response.readLine() == "WELCOME"
+	
+	if (args[0] == "--kill") {
+		request.println "KILL"
+		assert response.readLine() == "GOODBYE"		
+	} else {
+		def file = new File(args[0])
+		assert file.canRead()
+		def codeLines = 0
+		file.eachLine { codeLines++ }
+		
+		request.println "RUN ${codeLines}"
+		file.eachLine { request.println it }
+		assert response.readLine() == "RECIEVED"
+		
+		def status = response.readLine().split(" (?:: )?", 2)
+		if (status[0] == "SUCCESS")
+			response.eachLine { println it }
+		else if (status[0] == "EXECUTION-ERROR")
+			System.err.println "Runtime error: ${status[1]}"
+		else
+			System.err.println "Unknown error (status: ${status})"
+	}
+} finally {
+	client.close()
 }
-
-client.close()
