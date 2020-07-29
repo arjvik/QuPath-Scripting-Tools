839dd34154094326cece4cd569970531f98ffec7
Arjun Vikram <arjvik@gmail.com>
2020-07-28 22:06:48 -0500

    Improve RemoteCodeClient error handling

--- tools/RemoteCodeClient.groovy
+++ tools/RemoteCodeClient.groovy
@@ -1,3 +1,11 @@
+final PORT = 6666
+
+class ClientException extends Exception {
+	public ClientException(String message) {
+		super(message)
+	}
+}
+
 if (args == null || args.length == 0) {
 	System.err.println "Usage: groovy RemoteCodeClient.groovy path/to/script.groovy"
 	System.exit(1)
@@ -8,14 +16,24 @@ if (args == null || args.length == 0) {
 	System.exit(1)
 }
 
-final PORT = 6666
-
-def client = new Socket(InetAddress.localHost, PORT)
+Socket client
 try {
+	try {
+		client = new Socket(InetAddress.loopbackAddress, PORT)
+	} catch (ConnectException e) {
+		throw new ClientException("Unable to connect to localhost:${PORT}\n"+
+								  "Please ensure RemoteCodeServer is running in QuPath")
+	}
+	
 	def request = new PrintStream(client.outputStream)
 	def response = new BufferedReader(new InputStreamReader(client.inputStream))
 	
-	assert response.readLine() == "WELCOME"
+	def status = response.readLine()
+	if (status == null)
+		throw new ClientException("Unable to communicate with remote server\n"+
+								  "This can happen when you have started SSH forwarding,\n"+
+								  "but nothing is listening to port ${PORT} on the remote")
+	assert status == "WELCOME"
 	
 	if (args[0] == "--kill") {
 		request.println "KILL"
@@ -23,11 +41,9 @@ try {
 		println "Server exited properly"
 	} else {
 		def file = new File(args[0])
-		if (!file.isFile() || !file.canRead()) {
-			System.err.println "Unable to read file ${args[0]}"
-			System.err.println "Please make sure it exists and can be read"
-			System.exit(1)
-		}
+		if (!file.isFile() || !file.canRead())
+			throw new ClientException("Unable to read file ${args[0]}\n"+
+									  "Please make sure it exists and can be read")
 		
 		def codeLines = 0
 		file.eachLine { codeLines++ }
@@ -35,7 +51,7 @@ try {
 		file.eachLine { request.println it }
 		assert response.readLine() == "RECIEVED"
 		
-		def status = response.readLine().split(" (?:: )?", 2)
+		status = response.readLine().split(" (?:: )?", 2)
 		if (status[0] == "SUCCESS")
 			response.eachLine { println it }
 		else if (status[0] == "EXECUTION-ERROR")
@@ -43,6 +59,9 @@ try {
 		else
 			System.err.println "Unknown error (status: ${status})"
 	}
+} catch (ClientException e) {
+	System.err.println e.getMessage()
 } finally {
-	client.close()
+	if (client != null)
+		client.close()
 }
